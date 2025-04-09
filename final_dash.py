43041c0b6a0798f3bf24c2356dd2e18d6f6a21a7
yehyifan <74586762+yehyifan@users.noreply.github.com>
2025-04-09 18:45:15 +0100

    Update final_dash.py
    Link of Spotify_data.csv

--- final_dash.py
+++ final_dash.py
@@ -22,20 +22,12 @@ from urllib import request
 """# Load Data"""
 
 # Load dataset
-# dataset_url = "https://drive.google.com/uc?export=view&id=1Dx65HYqbI4xDlzkCe3ZaZ6gzyzwIvvvb"
-# name = 'Spotify_data.csv'
-# with request.urlopen(dataset_url) as f, open(name, 'w') as outf:
-#     outf.write(f.read().decode('ISO-8859-1'))
-# df = pd.read_csv(name)
-# df.head()
-
-from io import StringIO
-
-dataset_url = "https://drive.google.com/uc?export=download&id=1Dx65HYqbI4xDlzkCe3ZaZ6gzyzwIvvvb"
-
-with request.urlopen(dataset_url) as f:
-    content = f.read().decode('ISO-8859-1')
-    df = pd.read_csv(StringIO(content))
+dataset_url = "https://raw.githubusercontent.com/yehyifan/SpotiftDashApp/refs/heads/main/Spotify_data.csv"
+name = 'Spotify_data.csv'
+with request.urlopen(dataset_url) as f, open(name, 'w') as outf:
+    outf.write(f.read().decode('ISO-8859-1'))
+df = pd.read_csv(name)
+df.head()
 
 """# Data Preprocessing"""
 
